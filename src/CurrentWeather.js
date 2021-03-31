ba5b750e9d38f8de87268b790340fe8880151d8b
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-03-31 20:16:18 +0100

    Added api and connected location and weather description to the api

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
 import sun from "./sun.png";
+import axios from "axios";
 
 import CurrentTemp from "./CurrentTemp";
 
 import "./CurrentWeather.css";
 export default function CurrentWeather() {
   const [city, onCity] = useState(null);
-  const [mess, onMess] = useState(null);
+  const [cityUpdate, onCityUpdate] = useState(null);
+  const [description, onDescription] = useState(null);
 
   let weatherData = {
     date: "Thur 11 Mar",
-    location: "London",
     time: "19:00",
-    description: "Sunny",
   };
+
+  function showLocationInfo(response) {
+    console.log(response.data.weather[0].description);
+    onCityUpdate(response.data.name);
+    onDescription(response.data.weather[0].description);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    onMess(city);
+    let apiKey = "9949a4027ac8b116bb6aff55d501ba46";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showLocationInfo);
   }
 
   function updateCity(event) {
     onCity(event.target.value);
   }
+
+  //When the city name is submit the date, time and weather description change
 
   return (
     <div className="CurrentWeather">
@@ -33,7 +44,7 @@ export default function CurrentWeather() {
             type="search"
             className="form-control"
             placeholder="City"
-            autocomplete="off"
+            autoComplete="off"
             onChange={updateCity}
           />
           <div className="search-btn">
@@ -57,13 +68,13 @@ export default function CurrentWeather() {
           <div className="city-name">
             <i className="fas fa-map-marker-alt"></i>
             <span> </span>
-            <h2>{mess}</h2>
+            <h2>{cityUpdate}</h2>
           </div>
         </div>
         <h3>{weatherData.time}</h3>
         <img src={sun} alt="sun" />
         <div className="description">
-          <span>{weatherData.description}</span>
+          <span>{description}</span>
         </div>
         <div className="row">
           <CurrentTemp />
@@ -72,7 +83,7 @@ export default function CurrentWeather() {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
           fill="#fff"
-          fill-opacity="1"
+          fillOpacity="1"
           d="M0,128L60,112C120,96,240,64,360,85.3C480,107,600,181,720,186.7C840,192,960,128,1080,106.7C1200,85,1320,107,1380,117.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
         ></path>
       </svg>
