6b20c29c101040b70f28e170820de50dba122b89
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-04-02 18:24:51 +0100

    Working on formatting date

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormatDate from "./FormatDate";
 import "./CurrentWeather.css";
 
 export default function CurrentWeather() {
@@ -9,7 +8,7 @@ export default function CurrentWeather() {
 
   function showLocationInfo(response) {
     setWeatherData({
-      date: new Date(response.data.dt * 1000),
+      date: "Thur 8 Apr",
       time: "19:00",
       cityName: response.data.name,
       description: response.data.weather[0].description,
@@ -63,9 +62,7 @@ export default function CurrentWeather() {
           </div>
         </div>
 
-        <h1>
-          <FormatDate date={weatherData.date} />
-        </h1>
+        <h1>{weatherData.date}</h1>
         <div className="Location">
           <div className="city-name">
             <i className="fas fa-map-marker-alt"></i>
--- src/FormatDate.js
+++ src/FormatDate.js
@@ -1,28 +1,6 @@
 import React from "react";
 
 export default function FormatDate(props) {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  let month = months[props.date.getMonth()];
-  let todayDate = props.date.getDate();
-
-  return (
-    <div>
-      {} {todayDate} {month}
-    </div>
-  );
+  console.log(props.date);
+  return <div></div>;
 }
