ed9429edd665b28a3d6ae3850a266ced804da5d1
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-04-03 18:34:49 +0100

    Working on formatting date

--- src/FormatDate.js
+++ src/FormatDate.js
@@ -2,5 +2,27 @@ import React from "react";
 
 export default function FormatDate(props) {
   console.log(props.date);
-  return <div></div>;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let day = days[date.getDay()];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let todayDate = date.getDate();
+  return (
+    <div>
+      {day} {todayDate} {month}
+    </div>
+  );
 }
