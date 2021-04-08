6bd6f38078bb6ccf4bbdbe5c04af4f63b8307f9b
Hannah Osibodu <HannahOsibodu68@outlook.com>
2021-04-08 19:38:43 +0100

    Removing errors from links

--- src/Footer.js
+++ src/Footer.js
@@ -7,13 +7,21 @@ export default function Footer() {
     <div className="Footer">
       <small>
         <p>
-          <a href="https://github.com/Hannah6898/Weather-app" target="_blank">
+          <a
+            href="https://github.com/Hannah6898/Weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
             Open Souce code
           </a>
           <span> </span>
           by
           <span> </span>
-          <a href="https://www.hannahosibodu.com/" target="_blank">
+          <a
+            href="https://www.hannahosibodu.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Hannah Osibodu
           </a>
         </p>
